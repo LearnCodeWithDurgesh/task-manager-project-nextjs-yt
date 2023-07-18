2fa0efdb90da5f9662a7be9b6ac9446d5d5d7caa
durgesh tiwari <55891143+LearnCodeWithDurgesh@users.noreply.github.com>
2023-07-18 11:15:14 +0530

    login form

--- src/app/add-task/page.jsx
+++ src/app/add-task/page.jsx
@@ -1,14 +1,84 @@
 import React from "react";
-
+import loginSvg from "../../assets/login.svg";
+import Image from "next/image";
 export const metadata = {
   title: "Add Task : Work Manager",
 };
 
 const AddTask = () => {
   return (
-    <div className="flex justify-center">
-      <div className="border border-red-400 p-5">
-        <h1>Add your task here !!</h1>
+    <div className="grid grid-cols-12  justify-center">
+      <div className="col-span-4 col-start-5 p-5  shadow-sm">
+        <div className="my-8 flex justify-center">
+          <Image
+            src={loginSvg}
+            style={{
+              width: "50%",
+            }}
+          />
+        </div>
+        <h1 className="text-3xl text-center">Add your task here </h1>
+
+        <form action="#!">
+          {/* task title  */}
+          <div className="mt-4">
+            <label
+              htmlFor="task_title"
+              className="block text-sm font-medium mb-2"
+            >
+              Title
+            </label>
+            <input
+              type="text"
+              className="w-full p-3 rounded-3xl bg-gray-800 focus:ring-gray-400-100 border border-gray-800"
+              id="task_title"
+            />
+          </div>
+          {/* task CONENT  */}
+          <div className="mt-4">
+            <label
+              htmlFor="task_content"
+              className="block text-sm font-medium mb-2"
+            >
+              Content
+            </label>
+            <textarea
+              className="w-full p-3 rounded-3xl bg-gray-800 focus:ring-gray-400-100 border border-gray-800"
+              id="task_content"
+              rows={5}
+            />
+          </div>
+
+          {/* task status */}
+          <div className="mt-4">
+            <label
+              htmlFor="task_status"
+              className="block text-sm font-medium mb-2"
+            >
+              Status
+            </label>
+            <select
+              id="task_status"
+              className="w-full p-3 rounded-3xl bg-gray-800 focus:ring-gray-400-100 border border-gray-800"
+            >
+              <option value="none" selected disabled>
+                ---Select Status---
+              </option>
+              <option value="Pending">Pending</option>
+              <option value="Completed">Completed</option>
+            </select>
+          </div>
+
+          {/* button  actions */}
+          <div className="mt-4 flex justify-center">
+            <button className="bg-blue-600 py-2 px-3 rounded-lg hover:bg-blue-800">
+              Add Todo
+            </button>
+            <button className="bg-red-600 py-2 px-3 rounded-lg hover:bg-red-800 ms-3">
+              Clear
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
