c872289939bc94131bd1064ed6231ba9879a109d
durgesh tiwari <55891143+LearnCodeWithDurgesh@users.noreply.github.com>
2023-08-18 14:02:11 +0530

    fixed

--- src/app/api/current/route.js
+++ src/app/api/current/route.js
@@ -4,6 +4,11 @@ import { User } from "@/models/user";
 export async function GET(request) {
   const authToken = request.cookies.get("authToken")?.value;
   console.log(authToken);
+  if (!authToken) {
+    return NextResponse.json({
+      message: "User is not loggedin",
+    });
+  }
   const data = jwt.verify(authToken, process.env.JWT_KEY);
   console.log(data);
   const user = await User.findById(data._id).select("-password");
